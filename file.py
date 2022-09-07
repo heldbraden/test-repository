d351a43768cb82448fda88ed5dddf49acb1d0741
Sandstrom <evsandstrom@gmail.com>
2022-09-07 11:51:56 -0400

    Changed file.py

--- file.py
+++ file.py
@@ -1,1 +1,14 @@
-# Why are python comments like this :(((
+#A simple, yet polite, program for your pleasure :) 
+def greetings ():
+    print ("May I please have your name? ")
+    name = input()
+    print ("Welcome " + name + "! Thank you for your name. Are you doing well today? (Y/N)")
+    response = input() 
+    if response == "Y":
+        print ("I am glad to hear!")
+    elif response == "N": 
+        print ("I am sorry to hear that :(")
+    else: 
+        print ("I don't know what " + response + " means. Sorry.")
+
+greetings() 
